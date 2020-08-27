3bc4368e286957a9a7c23b8b170e3541ad75e4f4
TechnologyMan101 <65311278+TechnologyMan101@users.noreply.github.com>
2020-08-27 12:50:39 -0700

    Add files via upload

---
+++ Version 1.40/Pop!_OS Setup Script 1.40.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Pop!_OS Setup Script 1.40"
+echo "Created by Nathan Viroonchatapan"
+sleep 10
+sudo apt update -y
+sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers system76-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi pitivi supertux dconf-editor deja-dup gnome-todo gnome-sushi fonts-cantarell gnome-books
+sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
+sudo dpkg-reconfigure libdvd-pkg
+sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
+sudo add-apt-repository -y ppa:linuxuprising/java
+sudo apt install -y oracle-java14-installer
+java --version
+sleep 5
+java -version
+sleep 5
+sudo add-apt-repository -y ppa:mkusb/ppa
+sudo apt install -y mkusb mkusb-nox usb-pack-efi gparted
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+sudo apt install -y obs-studio
+sudo add-apt-repository -y ppa:minetestdevs/stable
+sudo apt install -y minetest
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update -y
+sudo apt install -y spotify-client
+sudo apt update -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo usermod -a -G cdrom $USER
+echo "Rebooting..."
+sleep 5
+sudo reboot
