0613288b89f4677975019845e14816e396a1b541
TechnologyMan101 <65311278+TechnologyMan101@users.noreply.github.com>
2020-09-16 00:40:49 -0700

    Update Pop!_OS Setup Script.sh

--- Pop!_OS Setup Script.sh
+++ Pop!_OS Setup Script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 finish () {
 	clear
-	tput setaf 1
+	tput setaf 10
+	echo "Done..."
+	tput setaf 9
 	echo "Rebooting..."
 	sleep 3
 	sudo reboot
@@ -64,20 +66,21 @@ do
 	clear
  	tput setaf 3
 	echo "=================================="
-	echo " --- Pop!_OS Setup Script 3.0 ---"
+	echo " --- Pop!_OS Setup Script 3.1 ---"
 	echo "=================================="
 	echo "Supported Pop!_OS Versions: 20.04 LTS"
-	tput setaf 2
+	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
+	tput setaf 10
 	echo "Script created by Nathan Viroonchatapan."
 	tput setaf 3
 	echo "You can open this script in a text editor to see packages to be installed in detail."
 	echo "Press 1 to install all packages in the script (Full Install/All User Packages)"
 	echo "Press 2 to install only the base packages in the script (Minimal Install/Essentials)"
-	tput setaf 1
+	tput setaf 9
 	echo "Press Q to quit"
 	tput setaf 3
 	echo "Enter your selection here and hit <return>..."
-	tput setaf 1
+	tput setaf 9
 	echo "System will automatically reboot after the script is run!!!"
 	echo "It is not recommended to run this script more than once!!!"
 	tput sgr0
@@ -88,10 +91,4 @@ do
 		q) exit;;
 		Q) exit;;
 	esac
-	clear
-	tput setaf 3
-	echo "Hit the <return> key to finish..."
-	tput sgr0
-	read answer
-	exit
 done
