731f13aae8f956543bd1d628ac6cfdb52f4da96c
TechnologyMan101 <65311278+TechnologyMan101@users.noreply.github.com>
2020-09-24 18:36:11 -0700

    Restructure and User Experience Improvements

--- Pop!_OS Setup Script.sh
+++ Pop!_OS Setup Script.sh
@@ -1,22 +1,71 @@
 #!/bin/bash
+# Start of Function Cluster
+mainmenu () {
+	clear
+ 	tput setaf 3
+	echo "=================================="
+	echo " --- Pop!_OS Setup Script 3.2 ---"
+	echo "=================================="
+	echo "Supported Pop!_OS Versions: 20.04 LTS"
+	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
+	tput setaf 10
+	echo "Script created by Nathan Viroonchatapan."
+	tput setaf 3
+	echo "You can open this script in a text editor to see packages to be installed in detail."
+	tput setaf 9
+	echo "System will automatically reboot after the script is run!!!"
+	echo "It is not recommended to run this script more than once!!!"
+	tput setaf 3
+	echo "Press 1 to perform a Full Install (All User Packages)"
+	echo "Press 2 to perform a Minimal Install (Essentials)"
+	tput setaf 9
+	echo "Press Q to quit"
+	tput sgr0
+	echo "Enter your selection followed by <return>:"
+	tput sgr0
+	read answer
+	case "$answer" in
+		1) full;;
+		2) minimal;;
+		q) quitscript;;
+		Q) quitscript;;
+	esac
+	badoption
+}
+quitscript () {
+	tput sgr0
+	clear
+	exit
+}
+badoption () {
+	clear
+	tput setaf 9
+	echo "Invalid Option!"
+	tput setaf 3
+	echo "Returning to Main Menu..."
+	tput sgr0
+	sleep 3
+}
 finish () {
 	clear
 	tput setaf 10
 	echo "Done..."
 	tput setaf 9
 	echo "Rebooting..."
+	tput sgr0
 	sleep 3
+	clear
 	sudo reboot
 }
 full () {
 	clear
 	tput setaf 3
 	echo "Full Install/All User Packages..."
+	tput sgr0
 	sleep 3
 	clear
-	tput sgr0
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers system76-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi gnome-sushi unoconv fonts-cantarell gnome-books krita gnome-clocks gimp
+	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers system76-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart speedtest-cli grub2-common neofetch network-manager-openvpn-gnome ffmpeg httraqt lsp-plugins tree audacity telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo pitivi gnome-sushi unoconv fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission
 	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
 	sudo dpkg-reconfigure libdvd-pkg
 	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
@@ -40,18 +89,18 @@ full () {
 	sudo apt full-upgrade -y
 	sudo apt autoremove -y
 	sudo apt autoclean -y
-	sudo usermod -a -G cdrom $USER
+	sudo usermod -aG cdrom $USER
 	finish
 }
 minimal () {
 	clear
 	tput setaf 3
 	echo "Minimal Install/Essentials..."
+	tput sgr0
 	sleep 3
 	clear
-	tput sgr0
 	sudo apt update -y
-	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gnome-tweaks gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup gnome-sushi unoconv fonts-cantarell
+	sudo apt install -y ubuntu-restricted-extras synaptic pavucontrol rhythmbox gparted p7zip-full p7zip-rar gnome-tweaks gpart network-manager-openvpn-gnome ffmpeg gufw dconf-editor deja-dup gnome-sushi unoconv fonts-cantarell htop
 	sudo apt install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-plugins-good libavcodec-extra gstreamer1.0-libav chromium-codecs-ffmpeg-extra libdvd-pkg
 	sudo dpkg-reconfigure libdvd-pkg
 	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
@@ -61,34 +110,10 @@ minimal () {
 	sudo apt autoclean -y
 	finish
 }
+# End of Function Cluster
+# Start of Main Script
 while true
 do
-	clear
- 	tput setaf 3
-	echo "=================================="
-	echo " --- Pop!_OS Setup Script 3.1 ---"
-	echo "=================================="
-	echo "Supported Pop!_OS Versions: 20.04 LTS"
-	echo "Script may prompt you or ask you for your password once in a while. Please monitor your computer until the script is done."
-	tput setaf 10
-	echo "Script created by Nathan Viroonchatapan."
-	tput setaf 3
-	echo "You can open this script in a text editor to see packages to be installed in detail."
-	echo "Press 1 to install all packages in the script (Full Install/All User Packages)"
-	echo "Press 2 to install only the base packages in the script (Minimal Install/Essentials)"
-	tput setaf 9
-	echo "Press Q to quit"
-	tput setaf 3
-	echo "Enter your selection here and hit <return>..."
-	tput setaf 9
-	echo "System will automatically reboot after the script is run!!!"
-	echo "It is not recommended to run this script more than once!!!"
-	tput sgr0
-	read answer
-	case "$answer" in
-		1) full;;
-		2) minimal;;
-		q) exit;;
-		Q) exit;;
-	esac
+	mainmenu
 done
+# End of Main Script
