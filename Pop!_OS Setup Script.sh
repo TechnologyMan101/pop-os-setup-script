852601ebec090f4c8d452b18531c5965a922a932
NP Viroonc <nathanviroonchatapan@outlook.com>
2021-11-14 06:46:08 -0800

    Fixeed bugs and updated Java to 17

--- Pop!_OS Setup Script.sh
+++ Pop!_OS Setup Script.sh
@@ -59,7 +59,7 @@ mainmenu () {
 	clear
  	tput setaf 3
 	echo "=================================="
-	echo " --- Pop!_OS Setup Script 4.5 ---"
+	echo " --- Pop!_OS Setup Script 4.6 ---"
 	echo "=================================="
 	echo "Supported Pop!_OS Versions (x86_64): 20.04 LTS, 21.04"
 	tput setaf 10
@@ -129,8 +129,8 @@ full () {
 	sudo apt install -y ubuntu-restricted-extras gnome-backgrounds ubuntu-gnome-wallpapers system76-wallpapers synaptic remmina bleachbit frozen-bubble musescore3 asunder brasero k3b pavucontrol pulseeffects rhythmbox shotwell solaar gnome-boxes gparted vlc p7zip-full p7zip-rar gnome-tweaks lame gpart grub2-common neofetch network-manager-openvpn-gnome ffmpeg webhttrack lsp-plugins tree telegram-desktop gufw easytag android-tools-adb android-tools-fastboot gnome-sound-recorder cheese nikwi supertux dconf-editor deja-dup gnome-todo gnome-sushi unoconv ffmpegthumbs fonts-cantarell gnome-books krita gnome-clocks gimp htop transmission curl git handbrake gtk-3-examples menulibre nautilus-admin python3-pip
 	sudo apt install -y libc6-i386 libx11-6:i386 libegl1-mesa:i386 zlib1g:i386 libstdc++6:i386 libgl1-mesa-dri:i386 libasound2:i386 libpulse0:i386
 	sudo add-apt-repository -y ppa:linuxuprising/java
-	echo oracle-java16-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
-	sudo apt install -y oracle-java16-installer --install-recommends
+	echo oracle-java17-installer shared/accepted-oracle-license-v1-2 select true | sudo /usr/bin/debconf-set-selections
+	sudo apt install -y oracle-java17-installer --install-recommends
 	java -version
 	sleep 3
 	sudo add-apt-repository -y ppa:mkusb/ppa
@@ -173,6 +173,11 @@ minimal () {
 	flatpak update -y
 	flatpak uninstall -y --unused --delete-data
 	pip3 install pip speedtest-cli -U
+	echo "Patching LSP icons..."
+	mkdir ~/.local/share/applications
+	echo "[Desktop Entry]
+	Hidden=true" > /tmp/1
+	find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 	finish
 }
 common () {
